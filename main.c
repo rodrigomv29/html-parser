d40d4f72ec961b97aafe981cf9b2188b13b95bc3
rodrigomv29 <43789876+rodrigomv29@users.noreply.github.com>
2023-10-24 15:32:03 -0400

    fgetc attempt

--- main.c
+++ main.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
-
+#include <stdlib.h>
 int main(){
-	printf("Hello World");
+	printf("Hello World\n");
+	FILE* file = fopen("demo.html", "r");
+	if(!file){
+		printf("OOPS!");
+		exit(-1);
+	}
+	char c;
+	printf("reading from file...\n");
+	while ( c=getc(file)!=EOF){
+		printf("We just read %s\n", c);
+        }
+	fclose(file);
+
 	return 1;
 }
